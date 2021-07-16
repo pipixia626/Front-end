7edad1a24d692aed0026744e3e0eafc6d97c3ff4
pipixia626 <814151675@qq.com>
2021-07-16 11:56:07 +0800

    finish stack To queue with pop push

---
+++ alogrithm/stack/StackToQueue.cpp
@@ -0,0 +1,31 @@
+/*
+/用两个栈来实现一个队列，完成队列的Push和Pop操作。 
+/队列中的元素为int类型。
+*/
+
+#include<iostream>
+#include<stack>
+
+class Solution{
+    private:
+    std::stack<int>stack1;
+    std::stack<int>stack2;
+    public:
+    void push(int node){
+      stack1.push(node);
+    }
+
+    int pop(){
+      int res;
+      if(stack2.empty()){
+          while (!stack1.empty())
+          {
+              stack2.push(stack1.top());
+              stack1.pop();
+          }
+      }
+      res=stack2.top();
+      stack2.pop();
+      return res;
+    }
+};
