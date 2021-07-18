73deaa3059b7db88f73b3b68d8b2b1fa5a6c31ab
pipixia626 <814151675@qq.com>
2021-07-18 15:38:22 +0800

    add stl heap sort

--- alogrithm/sort/selection/heap_sort.cpp
+++ alogrithm/sort/selection/heap_sort.cpp
@@ -1,22 +1,37 @@
+
 #include <vector>
 #include <iostream>
-
-class heap_sort
+#include <algorithm>
+#include <functional> // for greater<>
+using namespace std;
+int main()
 {
-public:
-    void swap(std::vector<int> &arr, int i, int j)
+    vector<float> bigs(10000, 0);
+    vector<float>::iterator it;
+    // Init vector data
+    for (it = bigs.begin(); it != bigs.end(); it++)
     {
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
+        *it = (float)rand() / 7; // random values;
     }
-
-    void print_cout(std::vector<int> arr)
+    cout << bigs.size() << endl;
+    make_heap(bigs.begin(), bigs.end(), greater<float>()); // The first one is the smallest one!
+    float ff;
+    for (int i = 0; i < 1000000000; i++)
     {
-        for (int i = 0; i < arr.size(); i++)
+        ff = (float)rand() / 7; //十亿个数都以随机数代替
+        if (ff > bigs.front())  // replace the first one ?
         {
-            std::cout << arr[i] << "->";
+            // set the smallest one to the end!
+            pop_heap(bigs.begin(), bigs.end(), greater<float>());
+            // remove the last/smallest one
+            bigs.pop_back();
+            // add to the last one
+            bigs.push_back(ff);
+            // mask heap again, the first one is still the smallest one
+            push_heap(bigs.begin(), bigs.end(), greater<float>());
         }
-        std::cout << std::endl;
     }
-};
+    // sort by ascent
+    sort_heap(bigs.begin(), bigs.end(), greater<float>());
+    return 0;
+}
