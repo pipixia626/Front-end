c09c99922bf2ab0dd80c7ee57df8973bb66039fd
pipixia626 <814151675@qq.com>
2021-07-31 12:20:34 +0800

    add removerNth

--- alogrithm/linklist/removeNthFromEnd.cpp
+++ alogrithm/linklist/removeNthFromEnd.cpp
@@ -1,42 +1,52 @@
-#include<iostream>
-struct ListNode {
+#include <iostream>
+struct ListNode
+{
     int val;
-    ListNode* next;
+    ListNode *next;
     ListNode() : val(0), next(nullptr) {}
     ListNode(int x) : val(x), next(nullptr) {}
-    ListNode(int x, ListNode* next) : val(x), next(next) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
-class Solution {
+class Solution
+{
 public:
-    ListNode* removeNthFromEnd(ListNode* head, int n) {
-        ListNode* fast = head;
-        ListNode* slow = head;
-        while (n--) {
-            fast = fast->next;
-        }
-        while (fast ->next!= nullptr) {
-            fast = fast->next;
-            slow = slow->next;
-        }
-        ListNode* cur = slow->next;
-        slow->next = slow->next->next;
-        delete(cur);
+    ListNode *removeNthFromEnd(ListNode *head, int n)
+    {
+        ListNode* dummyHead=new ListNode(0,head);
+        dummyHead->next=head;
 
-        return head;
+        ListNode*fast=head;
+        ListNode*slow=dummyHead;
+
+        for(int i=0;i<n;++i){
+            fast=fast->next;
+        }
+        while (fast)
+        {
+            fast=fast->next;
+            slow=slow->next;
+        }
+        slow->next=slow->next->next;
+        ListNode*res=dummyHead->next;
+        delete dummyHead;
+        return res;
+        
     }
 };
 
-int main() {
-    ListNode* list = new ListNode();
-    ListNode* cur = list;
-    for (int i = 1; i < 6; ++i) {
-        ListNode* Node = new ListNode(i);
+int main()
+{
+    ListNode *list = new ListNode();
+    ListNode *cur = list;
+    for (int i = 1; i < 6; ++i)
+    {
+        ListNode *Node = new ListNode(i);
         cur->next = Node;
         cur = Node;
     }
     list = list->next;
 
     Solution s;
-    ListNode* newList = s.removeNthFromEnd(list, 2);
-    std::cout << newList->next->val<<std::endl;
+    ListNode *newList = s.removeNthFromEnd(list, 2);
+    std::cout << newList->next->val << std::endl;
 }
