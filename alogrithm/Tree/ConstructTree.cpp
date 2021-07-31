d940d7bdce423aff9f05d889dfecf6826af90bd0
pipixia626 <814151675@qq.com>
2021-07-31 16:21:57 +0800

    add getNext and rebulid tree with inorder and preorder

---
+++ alogrithm/Tree/ConstructTree.cpp
@@ -0,0 +1,67 @@
+//根据前序遍历和中序遍历重建二叉树
+#include <iostream>
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+TreeNode *Construct(int *preorder, int *inorder, int length)
+{
+    if (preorder == nullptr || inorder == nullptr || length <= 0)
+    {
+        return nullptr;
+    }
+    return ConstructCore(preorder, preorder + length - 1, inorder, inorder + length - 1);
+}
+
+TreeNode *ConstructCore(
+    int *startPreorder, int *endPreorder,
+    int *startInorder, int *endInorder)
+{
+    //前序遍历序列的第一个数字是根节点的值
+
+    int rootValue = startPreorder[0];
+    TreeNode *root = new TreeNode();
+    root->val = rootValue;
+
+    if (startPreorder == endPreorder)
+    {
+        if (startInorder == endInorder && *startPreorder == *startInorder)
+            return root;
+        else
+            throw ::std::exception("Invalid input.");
+    }
+
+    //在中序遍历序列中找到根节点的值
+
+    int *rootInorder = startInorder;
+    while (rootInorder <= endInorder && *rootInorder != rootValue)
+    {
+        ++rootInorder;
+    }
+
+    if (rootInorder == endInorder && *rootInorder != rootValue)
+    {
+        throw ::std::exception("Invalid input.");
+    }
+
+    int leftLength = rootInorder - startInorder;
+    int *leftPreorderENd = startPreorder + leftLength;
+    if (leftLength > 0)
+    {
+        //构建左子树
+        root->left = ConstructCore(startPreorder + 1, leftPreorderENd, startInorder, rootInorder - 1);
+    }
+    if (leftLength < endPreorder - startPreorder)
+    {
+        //构建右子树
+        root->right = ConstructCore(leftPreorderENd + 1, endPreorder, rootInorder + 1, endInorder);
+    }
+
+    return root;
+}
---
+++ alogrithm/Tree/GetNext.cpp
@@ -0,0 +1,46 @@
+// 中序遍历中某个节点的下一个节点
+
+#include <iostream>
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode *parent;
+    TreeNode() : val(0), left(nullptr), right(nullptr),parent(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr),parent(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right,TreeNode*parent) : val(x), left(left), right(right),parent(parent) {}
+};
+
+TreeNode *GetNext(TreeNode *pNode)
+{
+    if (pNode == nullptr)
+    {
+        return nullptr;
+    }
+    TreeNode *pNext = nullptr;
+    //这个节点有右节点
+    if (pNode->right != nullptr)
+    {
+        TreeNode *pRight = pNode->right;
+        while (pRight->left != nullptr)
+            pRight = pRight->left;
+
+        pNext = pRight;
+    }
+    //没有右节点,但有父节点
+    else if(pNode->parent!=nullptr){
+        //获取当前节点
+        TreeNode*pCurrent=pNode;
+        //获取当前节点的父节点
+        TreeNode*pParent=pNode->parent;
+        
+        while(pParent!=nullptr&&pCurrent==pParent->right){
+            //找个这个节点的
+            pCurrent=pParent;
+            pParent=pParent->parent;
+        }
+        pNext=pParent;
+    }
+    return  pNext;
+}
