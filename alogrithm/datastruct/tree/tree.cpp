163c2d106a762cd031c4510bb435d6c24ddb0053
pipixia626 <814151675@qq.com>
2021-08-02 09:37:31 +0800

    apdate tree with no recursion

--- alogrithm/datastruct/tree/tree.cpp
+++ alogrithm/datastruct/tree/tree.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
+#include <stack>
 struct BinaryTreeNode
 {
     int val;
     BinaryTreeNode *left;
     BinaryTreeNode *right;
-    BinaryTreeNode():val(0),left(nullptr),right(nullptr){}
-    BinaryTreeNode(int x):val(x),left(nullptr),right(nullptr){}
-    BinaryTreeNode(int x,BinaryTreeNode* left,BinaryTreeNode* right):val(0),left(left),right(right){}
-    
+    BinaryTreeNode() : val(0), left(nullptr), right(nullptr) {}
+    BinaryTreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    BinaryTreeNode(int x, BinaryTreeNode *left, BinaryTreeNode *right) : val(0), left(left), right(right) {}
 };
 
 class Tree
@@ -16,41 +16,117 @@ private:
     BinaryTreeNode *root;
 
 public:
-    void preOrder(BinaryTreeNode *root)
+    void preOrder_recursion(BinaryTreeNode *root)
     {
         if (root == nullptr)
             return;
         std::cout << root->val << std::endl;
-        preOrder(root->left);
-        preOrder(root->right);
+        preOrder_recursion(root->left);
+        preOrder_recursion(root->right);
     }
 
-    void inOrder(BinaryTreeNode *root)
+    void inOrder_recursion(BinaryTreeNode *root)
     {
         if (root == nullptr)
             return;
-        inOrder(root->left);
+        inOrder_recursion(root->left);
         std::cout << root->val << std::endl;
-        inOrder(root->right);
+        inOrder_recursion(root->right);
     }
 
-    void postOrder(BinaryTreeNode *root)
+    void postOrder_recursion(BinaryTreeNode *root)
     {
         if (root == nullptr)
             return;
-        inOrder(root->left);
-        inOrder(root->right);
+        inOrder_recursion(root->left);
+        inOrder_recursion(root->right);
         std::cout << root->val << std::endl;
+    }
+
+    void preOrder_no_recursion(BinaryTreeNode *root)
+    {
+        if (root == nullptr)
+            return;
+        std::stack<BinaryTreeNode *> stackTree;
+        stackTree.push(root);
+        while (!stackTree.empty())
+        {
+            auto node = stackTree.top();
+            stackTree.pop();
+            std::cout << node->val << std::endl;
+            if (node->right != nullptr)
+                stackTree.push(node->right);
+            if (node->left != nullptr)
+                stackTree.push(node->left);
+        }
+    }
+
+    void inOrder_no_recursion(BinaryTreeNode *root)
+    {
+        if (root == nullptr)
+            return;
+        auto cur = root;
+        std::stack<BinaryTreeNode *> stackTree;
+        while (!stackTree.empty() || cur != nullptr)
+        {
+            while (cur != nullptr)
+            {
+                stackTree.push(cur);
+                cur = cur->left;
+            }
+            auto node = stackTree.top();
+            stackTree.pop();
+            std::cout << node->val << std::endl;
+
+            if (node->right != nullptr)
+            {
+                cur = node->right;
+            }
+        }
+    }
+
+    void postOrder_no_recursion(BinaryTreeNode *root)
+    {
+        if (root == nullptr)
+            return;
+        std::stack<BinaryTreeNode *> inTree;
+        std::stack<BinaryTreeNode *> resTree;
+        inTree.push(root);
+        while (!inTree.empty())
+        {
+            auto node = inTree.top();
+            inTree.pop();
+            resTree.push(node);
+            if (node->left != nullptr)
+            {
+                inTree.push(node->left);
+            }
+            if (node->right != nullptr)
+            {
+                inTree.push(node->right);
+            }
+        }
+        while (!resTree.empty())
+        {
+            auto it = resTree.top();
+            resTree.pop();
+            std::cout << it->val << std::endl;
+        }
     }
     //获取长度
-    int depth(BinaryTreeNode *root){
-        int dep1=0,dep2=0;
-        if(root==nullptr)return;
-        else{
-            dep1=depth(root->left);
-            dep2=depth(root->right);
-            if(dep1>dep2)return dep1+1;
-            else return dep2+1;
+    int depth(BinaryTreeNode *root)
+    {
+        int dep1 = 0, dep2 = 0;
+        if (root == nullptr)
+            return;
+        else
+        {
+            dep1 = depth(root->left);
+            dep2 = depth(root->right);
+            if (dep1 > dep2)
+                return dep1 + 1;
+            else
+                return dep2 + 1;
         }
     }
 };
