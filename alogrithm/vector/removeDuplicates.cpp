92467cd41785374ed8155e46acb84d9794154776
pipixia626 <814151675@qq.com>
2021-08-31 21:08:13 +0800

    add 去重序列和最长连续序列

---
+++ alogrithm/vector/removeDuplicates.cpp
@@ -0,0 +1,37 @@
+/*给你一个有序数组 nums ，
+请你 原地 删除重复出现的元素，使每个元素 只出现一次 
+，返回删除后数组的新长度。
+不要使用额外的数组空间，你必须在 原地 修改输入数组 并在使用 O(1) 额外空间的条件下完成。
+*/
+#include <vector>
+using namespace std;
+class Solution
+{
+public:
+    //0 1 1 2 2 3
+    //0 1 2 3 4 5
+    
+    //0 1 2 2 2 3 slow=1 fast=3 slow=2 fast=4 slow=2,fast=5
+    
+    // 0 1 2 3 2 3 slow=3 fast=6
+    int removeDuplicates(vector<int> &nums)
+    {
+        int n = nums.size();
+        if (n == 0)
+            return 0;
+        int slow = 0, fast = 1;
+        while (fast < n)
+        {
+            if (nums[fast] != nums[slow])
+            {
+                if (fast - slow > 1)
+                {
+                    nums[slow + 1] = nums[fast];
+                }
+                slow++;
+            }
+            fast++;
+        }
+        return slow + 1;
+    }
+};
