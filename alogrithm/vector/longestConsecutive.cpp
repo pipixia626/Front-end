09aac89454340298132242bc80a1dc59cd7c9641
pipixia626 <814151675@qq.com>
2021-09-12 13:19:59 +0800

    update longestConsecutive

--- alogrithm/vector/longestConsecutive.cpp
+++ alogrithm/vector/longestConsecutive.cpp
@@ -3,6 +3,10 @@
 #include <algorithm>
 #include <vector>
 using namespace std;
+//找连续的方法有从这个数前面开始找和从这个数后面开始找
+//首先判断前一个数存在不存在
+//存在则跳过
+//不存在则从这个数后面开始找
 class Solution
 {
 public:
@@ -17,10 +21,11 @@ public:
 
         for (const int &num : num_set)
         {
-            if (!num_set.count(num - 1))
+            if (!num_set.count(num - 1))//前一个数不存在
             {
                 int currentNum = num;
                 int currentStreak = 1;
+                //从这个数往后面开始找
                 while (num_set.count(currentNum + 1))
                 {
                     currentNum += 1;
