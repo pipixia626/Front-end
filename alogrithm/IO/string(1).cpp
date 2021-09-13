364681e49fec88c997e0e9f59c5fc0c594ab8ee4
pipixia626 <814151675@qq.com>
2021-09-13 21:43:33 +0800

    add Io string input

--- alogrithm/IO/string(1).cpp
+++ alogrithm/IO/string(1).cpp
@@ -2,3 +2,27 @@
 #include <vector>
 #include<string>
 using namespace std;
+//多个测试用例，每个测试用例一行
+//输入：每行通过逗号 ‘，’隔开，有n个字符
+
+//输出:处理好的输入
+
+#include<iostream>
+#include<string>
+#include<vector>
+#include<sstream>
+
+using namespace std;
+int main(){
+    string s,str;
+    vector<string>strs;
+    while (getline(cin,s))
+    {
+        stringstream ss(s);
+        while (getline(ss,str,','))
+        {
+           strs.push_back(str);
+        }
+    }
+    
+}
