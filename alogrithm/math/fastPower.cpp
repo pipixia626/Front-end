f89a7073760703efd91440a8a363f5e4092513c8
pipixia626 <814151675@qq.com>
2021-09-20 16:57:32 +0800

    update fastpow

--- alogrithm/math/fastPower.cpp
+++ alogrithm/math/fastPower.cpp
@@ -38,12 +38,27 @@ T qpow(T a, long long n)
     T ans = 1; // 赋值为乘法单位元，可能要根据构造函数修改
     while (n)
     {
-        if (n & 1)//这里是为了幂值不为偶数
+        if (n & 1)         //这里是为了幂值不为偶数
             ans = ans * a; // 这里就最好别用自乘了，不然重载完*还要重载*=，有点麻烦。
         n >>= 1;
         a = a * a;
     }
     return ans;
+}
+//二分法求
+double myPow(double x, int n)
+{
+    long long N = n;
+    return N >= 0 ? Binary_Mul(x, N) : 1.0 / Binary_Mul(x, N);
+}
+double Binary_Mul(double x, long long n)
+{
+    if (n == 0)
+        return 1.0;
+    double y = Binary_Mul(x, n / 2);
+    // 0 1 2 4 <->    1->(1*1)*2=2->2*2*2=8->8*8=64
+    // 0 1 3   <->    1->(1*1)*2=2->2*2*2=8
+    return n % 2 == 0 ? y * y : y * y * x;
 }
 //递归快速幂取模
 int main()
